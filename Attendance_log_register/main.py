823567f0da7309ac9d84a60793c98353679556c2
rajesh mishra <rajeshmishra847410@gmail.com>
2025-02-09 23:39:25 +0530

    add login and register page

--- Attendance_log_register/main.py
+++ Attendance_log_register/main.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def index(name=None):
     return render_template('index.html', person=name)
 
+#Login
 @app.route('/login')
 def login(name=None):
     return render_template('login.html', person=name)
+#Register
+@app.route('/register')
+def register():
+    return render_template('register.html')
